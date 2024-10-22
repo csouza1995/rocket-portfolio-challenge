da9d01532e3384b5e9c39412cb86576d90b2235b
Carlos Souza <carlos@i-databi.com.br>
2024-10-22 07:47:39 -0300

    feat: Estrutura iniciall

---
+++ README.md
@@ -0,0 +1,7 @@
+#
+
+## Turn On
+
+```php
+php -S localhost:8888 -d auto_prepend_file=server.php
+```
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carlo Souza - Rocket Portfolio Challenge</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-800">
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
